3a8582c3abfc2e1133e649ffcc750d6a386a25b6
Marcio Tavares <marcio.tavares@sefin.fortaleza.ce.gov.br>
2025-02-12 16:37:29 -0300

    query start analytics

--- sql/-- SQLite -- Olist.sql
+++ sql/-- SQLite -- Olist.sql
@@ -1,2 +1,11 @@
--- SQLite --
-SELECT * FROM tb_customers
+-- SQLite -- Delimitando a base ativa e o tempo de historico - granularidade
+SELECT COUNT(DISTINCT T2.seller_id),
+       MAX(T1.order_approved_at),
+       MIN(T1.order_approved_at)
+
+FROM tb_orders AS T1
+
+LEFT JOIN tb_order_items AS T2
+ON T1.order_id = T2.order_id
+
+WHERE T1.order_approved_at BETWEEN '2017-06-01' AND '2018-06-01'
---
+++ sql/-- SQLite -- Olist_2.sql
@@ -0,0 +1,12 @@
+-- SQLite -- Delimitando a base ativa e o tempo de historico - granularidade
+SELECT T2.seller_id,
+SUM( T2.price ) AS receita_total,
+COUNT(DISTINCT T1.order_id) AS qtd_pedidos,
+COUNT(T2.product_id) AS qtd_produtos,
+COUNT(DISTINCT T2.product_id) AS dist_qtd_produtos,
+
+FROM tb_orders AS T1
+LEFT JOIN tb_order_items AS T2
+ON T1.order_id = T2.order_id
+WHERE T1.order_approved_at BETWEEN '2017-06-01' AND '2018-06-01'
+GROUP BY T2.seller_id
