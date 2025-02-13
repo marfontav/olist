76e38934f4638758eb9e1ea2079d309e59c51f27
Marcio Tavares <marcio.tavares@sefin.fortaleza.ce.gov.br>
2025-02-13 15:21:53 -0300

    start segment rfv

--- sql/-- SQLite -- Olist_1.sql
+++ sql/-- SQLite -- Olist_1.sql
@@ -2,10 +2,7 @@
 SELECT COUNT(DISTINCT T2.seller_id),
        MAX(T1.order_approved_at),
        MIN(T1.order_approved_at)
-
 FROM tb_orders AS T1
-
 LEFT JOIN tb_order_items AS T2
 ON T1.order_id = T2.order_id
-
-WHERE T1.order_approved_at BETWEEN '2017-06-01' AND '2018-06-01'
+WHERE T1.order_approved_at BETWEEN '2017-06-01' AND '2018-06-01';
